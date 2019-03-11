fd23f37c03b2f122714186fa86fdafd769aabaaa
Vokmilk <shadowcool12@gmail.com>
2019-03-11 23:17:50 +0300

    feat: add count of love triangules

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  preferences.unshift('0');
+  var triangles = 0;
+    for (var i = 1; i < preferences.length; i++) 
+    {
+      var p=preferences[i]
+      var k=preferences[p]
+          if ( preferences[i] == p && preferences[p] == k && preferences[k] == i && i !== k && k !== p && i !== p)
+            triangles++
+        }
+     return triangles/3;
 };
